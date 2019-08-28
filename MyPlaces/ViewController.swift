a0791b9b2401203a3ba1910dc80c138993d78bbd
Adedal <nikolaymoroz11@mail.ru>
2019-08-28 17:01:48 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Николай Лазарски on 28/08/2019.
+//  Copyright © 2019 Adedal Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
